653338104515a5e11755f3e17214516a6aa10eb8
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-01-23 20:58:25 +0200

    added meta, corrected modal.js

--- js/modal.js
+++ js/modal.js
@@ -5,11 +5,31 @@
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", openModal);
+  refs.closeModalBtn.addEventListener("click", closeModal);
+  refs.modal.addEventListener("click", onBackdropClick);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-    body.classList.toggle("no-scroll");
+  function closeModal() {
+    window.removeEventListener("keydown", onEscButton);
+    refs.modal.classList.add("is-hidden");
+    document.body.classList.remove("no-scroll");
+  }
+
+  function openModal() {
+    window.addEventListener("keydown", onEscButton);
+    refs.modal.classList.remove("is-hidden");
+    document.body.classList.add("no-scroll");
+  }
+
+  function onEscButton(event) {
+    if (event.code === "Escape") {
+      closeModal();
+    }
+  }
+
+  function onBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+      closeModal();
+    }
   }
 })();
